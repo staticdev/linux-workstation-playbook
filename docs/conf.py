f5b70286bd747b4d1d8dae4b89aabb53f7e3b9ab
staticdev <staticdev-support@protonmail.com>
2022-03-15 18:15:22 +0100

    Documentation fixes

--- docs/conf.py
+++
@@ -1,9 +0,0 @@
-"""Sphinx configuration."""
-from datetime import datetime
-
-
-project = "Linux Workstation Playbook"
-author = "staticdev"
-copyright = f"{datetime.now().year}, {author}"
-extensions = ["sphinx.ext.autodoc", "sphinx.ext.napoleon"]
-autodoc_typehints = "description"
