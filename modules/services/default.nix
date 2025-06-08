c0b76bd1cea49863e3635fcd80aecca35a0c7bc1
staticdev <staticdev-support@proton.me>
2025-06-08 18:46:31 +0200

    Add *arr services

--- modules/services/default.nix
+++ modules/services/default.nix
@@ -4,6 +4,10 @@
     enable = lib.mkEnableOption "Settings and services for the homelab";
   };
   imports = [
+    ./arr/prowlarr
+    ./arr/bazarr
+    ./arr/sonarr
+    ./arr/radarr
     ./deluge
     ./jellyfin
     ./wireguard-netns
